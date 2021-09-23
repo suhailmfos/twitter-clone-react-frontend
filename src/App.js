d3e89b9b3f2d79086432d962627cbb429984c6fa
Suhail <suhailmfos@gmail.com>
2021-09-23 16:42:31 +0530

    frontend twitter commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the beginning</h1>
+      </div>
+    )
+  }
+}
